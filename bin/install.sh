cc787119a3e605b50cc3003b263e4ab66ae8ad2d
Alexei Bushuev <alexeib.2018@gmail.com>
2019-01-22 08:32:03 +0300

    fixed install

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-virtualenv ../.venv --python=python2
+rm -r -f ../.venv
+rm -r -f ../barcodes
+virtualenv ../.venv --python=python3
 ../.venv/bin/pip install pdf417gen
 mkdir ../barcodes
 chmod 777 ../barcodes
