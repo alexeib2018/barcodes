7c564d1fdf52775c3472e62b46ae425c62ac357a
Alexei Bushuev <alexeib.2018@gmail.com>
2019-02-16 13:20:07 +0300

    developed barcode generation view

--- barcodes/urls.py
+++ barcodes/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('jpg/', views.jpg, name='jpg'),
+    path('png/', views.png, name='png'),
 ]
--- barcodes/views.py
+++ barcodes/views.py
@@ -1,4 +1,6 @@
+from pdf417gen import encode, render_image
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
@@ -6,6 +8,13 @@ def index(request):
     return render(request, 'barcodes/index.html', context)
 
 
-def jpg(request):
-    context = {}
-    return render(request, 'barcodes/jpg.html', context)
+def png(request):
+    text = request.POST.get('text', request.GET.get('text', ''))
+
+    codes = encode(text)
+
+    # Generate barcode as image
+    image = render_image(codes)  # Pillow Image object
+    response = HttpResponse(content_type='image/png')
+    image.save(response, 'png')
+    return response
